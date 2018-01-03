b0415916c933e51c0ae854823bc7834ef783d0ca
Aguno <Aguno@naver.com>
2018-01-03 17:48:55 +0900

    fix read contacts.final

--- app/src/main/java/com/example/user/cs496_p1_t3/freefrag.java
+++ app/src/main/java/com/example/user/cs496_p1_t3/freefrag.java
@@ -1,20 +1,15 @@
 package com.example.user.cs496_p1_t3;
 
 import android.app.Fragment;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.File;
-import java.io.IOException;
 
 /**
  * Created by user on 2017-12-27.
@@ -25,91 +20,59 @@ public class freefrag extends Fragment {
     public freefrag(){
 
     }
-    MediaRecorder myAudioRecorder = null;
-    ImageButton play,stop,record;
-    boolean starting = false;
-
-
+    Button button3;
+    EditText NUMBER1;
+    EditText NUMBER2;
+    TextView RESULT;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedlnstanceState){
 
         View view = inflater.inflate(R.layout.freefrag, container, false);
-        play = (ImageButton)view.findViewById(R.id.play);
-        stop = (ImageButton)view.findViewById(R.id.stop);
-        record = (ImageButton)view.findViewById(R.id.record);
 
-        play.setEnabled(false);
-        stop.setEnabled(false);
-        record.setEnabled(true);
-
-
-        record.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                if(starting==false){
-                    if(myAudioRecorder == null) myAudioRecorder = new MediaRecorder();
-                    else myAudioRecorder.reset();
-                }
-
-
-                myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-                myAudioRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-                myAudioRecorder.setOutputFile(getContext().getFilesDir()+"/audio.3gp");
-               // if(myAudioRecorder!=null) myAudioRecorder.reset();
-                try {
-                    myAudioRecorder.prepare();
-                    myAudioRecorder.start();
-                } catch (IllegalStateException ise) {
-                } catch (IOException ioe) {
-
-                }
-                record.setEnabled(false);
-                stop.setEnabled(true);
-
-                Toast.makeText(getContext(), "Start recording", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        stop.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                myAudioRecorder.stop();
-                myAudioRecorder.release();
-                myAudioRecorder = null;
-                stop.setEnabled(false);
-                record.setEnabled(true);
-                play.setEnabled(true);
-                Toast.makeText(getContext(), "Finished recording", Toast.LENGTH_LONG).show();
-            }
-
-        });
-
-        play.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v){
-                MediaPlayer mediaplayer = new MediaPlayer();
-
-                try {
-                    mediaplayer.setDataSource(getContext().getFilesDir()+"/audio.3gp");
-                    mediaplayer.prepare();
-                    mediaplayer.start();
-
-                    Toast.makeText(getContext(), "Playing Audio", Toast.LENGTH_LONG).show();
-                } catch (IOException e) {
-                }
-
-
-            }
-        });
-
-
-
-
+        EditText NUMBER1 =(EditText)view.findViewById(R.id.NUMBER1);
+        EditText NUMBER2 =(EditText)view.findViewById(R.id.NUMBER2);
+        TextView RESULT = (TextView) view.findViewById(R.id.RESULT);
 
         return view;
     }
+    public  void addclick(View v){
+
+        int n1 = Integer.parseInt(NUMBER1.getText().toString());
+        int n2 = Integer.parseInt(NUMBER2.getText().toString());
+        RESULT.setText(Integer.toString(n1+n2));
+        if(n1==496 & n2==496){
+            Intent intent = new Intent(getActivity(), Diary.class);
+            startActivity(intent);
+
+        }
+
+
+    }
+
+    public  void subclick(View v){
+        int n1 = Integer.parseInt(NUMBER1.getText().toString());
+        int n2 = Integer.parseInt(NUMBER2.getText().toString());
+        RESULT.setText(Integer.toString(n1-n2));
+
+
+    }
+
+    public  void mulclick(View v){
+        int n1 = Integer.parseInt(NUMBER1.getText().toString());
+        int n2 = Integer.parseInt(NUMBER2.getText().toString());
+        RESULT.setText(Integer.toString(n1*n2));
+
+
+    }
+
+    public  void divclick(View v){
+        int n1 = Integer.parseInt(NUMBER1.getText().toString());
+        int n2 = Integer.parseInt(NUMBER2.getText().toString());
+        RESULT.setText(Integer.toString(n1/n2));
+
+
+    }
+
+
 }
